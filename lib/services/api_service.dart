f5b0b65e03e95e046360b633e4d2a138f751471f
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2023-01-12 18:31:12 +0900

    api

---
+++ lib/services/api_service.dart
@@ -0,0 +1,8 @@
+class ApiService {
+  final String baseUrl = "https://webtoon-crawler.nomardcoders.workers.dev";
+  final String today = "today";
+
+  void getTodaysToons() {
+    print("getTodaysToons");
+  }
+}
