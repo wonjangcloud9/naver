f60124f42cb8c96e9b7dd9f7c9ce89989d806a1d
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2023-01-14 22:54:31 +0900

    test App

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -9,4 +9,30 @@ class ApiService {
   void getToon(String toonId) {
     print("getToon");
   }
+
+  void getToonEpisodes(String toonId) {
+    print("getToonEpisodes");
+  }
+
+  void getToonEpisode(String toonId, String episodeId) {
+    print("getToonEpisode");
+  }
+
+  void getToonEpisodeImages(String toonId, String episodeId) {
+    print("getToonEpisodeImages");
+  }
+
+  void getToonEpisodeImage(String toonId, String episodeId, String imageId) {
+    print("getToonEpisodeImage");
+  }
+
+  void getToonEpisodeImageByIndex(
+      String toonId, String episodeId, int imageIndex) {
+    print("getToonEpisodeImageByIndex");
+  }
+
+  void getToonEpisodeImageByIndexRange(
+      String toonId, String episodeId, int imageIndex, int imageCount) {
+    print("getToonEpisodeImageByIndexRange");
+  }
 }
